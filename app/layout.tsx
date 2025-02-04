531c9c20e1e9c516300fe362c4a6913d587672db
azymv <timazcimaz@gmail.com>
2025-02-04 11:42:32 +0300

    changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
+  title: 'AI PM Course',
+  description: 'by Tim Azimov',
 }
 
 export default function RootLayout({
